c88acd23a3c20d1880e3283f4bb4952d7aadc265
pawebas155 <pawebas155@student.polsl.pl>
2020-01-19 23:59:43 +0100

    add parsing json to object

--- app/src/main/java/pl/polsl/pam/reversedweatherforecast/MainActivity.kt
+++ app/src/main/java/pl/polsl/pam/reversedweatherforecast/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.TextView
+import com.beust.klaxon.Klaxon
+import pl.polsl.pam.reversedweatherforecast.ServerEntity.ServerForecast
+import pl.polsl.pam.reversedweatherforecast.ServerEntity.TestForecast
 import java.net.URL
 
 class MainActivity : AppCompatActivity() {
@@ -17,10 +20,28 @@ class MainActivity : AppCompatActivity() {
 
         button.setOnClickListener {
             Thread {
+                //api.openweathermap.org/data/2.5/forecast?id={city ID} //wyszukiwanie po id
                 val response =
                     URL("https://api.openweathermap.org/data/2.5/forecast?q=Gliwice,PL&APPID=4749cf6173631a815735a7b0b88aeef7").readText()
 
+
+                try{
+                    var result = Klaxon().parse<TestForecast.TestForecastInfo>(response)
+
+                    var s = 10 + 34
+                }
+                catch (ex: Exception){
+
+                }
+
+
+
+                //var gson = Gson()
+                //var serverForecast = gson?.fromJson(response, ServerForecast.ForecastInfo::class.java)
+
+
                 runOnUiThread {
+
                     textView.text = response
                 }
             }.start()
---
+++ app/src/main/java/pl/polsl/pam/reversedweatherforecast/ServerEntity/TestForecast.kt
@@ -0,0 +1,69 @@
+package pl.polsl.pam.reversedweatherforecast.ServerEntity
+
+class TestForecast {
+
+    data class TestForecastInfo(
+    val city: City,
+    val cnt: Int,
+    val cod: String,
+    val list: List<X>,
+    val message: Int
+)
+
+data class City(
+    val coord: Coord,
+    val country: String,
+    val id: Int,
+    val name: String,
+    val sunrise: Int,
+    val sunset: Int,
+    val timezone: Int
+)
+
+data class Coord(
+    val lat: Double,
+    val lon: Double
+)
+
+data class X(
+    val clouds: Clouds,
+    val dt: Int,
+    val dt_txt: String,
+    val main: Main,
+    val sys: Sys,
+    val weather: List<Weather>,
+    val wind: Wind
+)
+
+data class Clouds(
+    val all: Int
+)
+
+data class Main(
+    val feels_like: Double,
+    val grnd_level: Int,
+    val humidity: Int,
+    val pressure: Int,
+    val sea_level: Int,
+    val temp: Double,
+    val temp_kf: Double,
+    val temp_max: Double,
+    val temp_min: Double
+)
+
+data class Sys(
+    val pod: String
+)
+
+data class Weather(
+    val description: String,
+    val icon: String,
+    val id: Int,
+    val main: String
+)
+
+data class Wind(
+    val deg: Int,
+    val speed: Double
+)
+}
